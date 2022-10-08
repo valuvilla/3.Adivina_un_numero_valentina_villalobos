3924db2bcf16b46b7a1848fcf6292abd96f1336d
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-08 12:37:55 +0000

    h

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import random
 import sys
-numero = random.randint(0,100)
+n1 = random.randint(0,100)
 print(numero)
 #P1: Captar un numero
-while True:
-  n2=input("Introduce un número entre 0 y 99: ")
-  try:
-    n2=int(n2)
-  except:
-    pass
-  else:
-    if 1 <= n2 <= 99:
-      break
-print("El número es válido") while True:
+def intentos(n1):
+  
+if __name___=="__main__":
+  print(intento())
   
