ce6bf1616f740fd86793a57cc579dce546d61a85
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-09 10:52:32 +0000

    intebyo

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import random
 import sys
+
+
 n1 = random.randint(0,100)
-print(numero)
+n1= int(n1)
+print(n1)
+
 #P1: Captar un numero
-def intentos(n1):
+def adivina_numero(n1):
+    while True:
+      #Creamos un bucle infinito
+
+      #Pedimos por pantalla un número
+      n2=input("Introduce un número entero entre 0 y 99: ")
+      try:
+        n2=int(n2) and 0<=n2<=99
+      except:
+        pass
+      if int(n2)<n1:
+        print("demasiado pequeño")
+      elif int(n2)>n1:
+        print("Demasiado grande")
+      else:
+        print("has ganado")
+        break
+    print("lo hemos conseguido")  
+    sys.exit()
+    print("fin del juego")
+
+
+    
+
+    
   
-if __name___=="__main__":
-  print(intento())
+
+
+
+
+if __name__=="__main__":
+  adivina_numero(n1)
   
