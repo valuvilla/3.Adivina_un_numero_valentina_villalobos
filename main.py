ff6496355886239ef910c992c3e02440f7621e96
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-10 19:38:35 +0000

    contar intentos

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ print(n1)
 
 #P1: Captar un numero
 def intentos(n1): 
-  def adivina_numero(n1): 
-     
+  intento = 0
+  def adivina_numero(n1):
+     intento = 0
      while True:
       #Creamos un bucle infinito
         n2=input("Introduce un número entero entre 0 y 99: ")
+       
+        intento = intento + 1
+       
         try:
             n2 = int(n2)
         except:
@@ -22,16 +26,22 @@ def intentos(n1):
             if 0<=n2<=100:
               if int(n2)<n1:
                 print("demasiado pequeño")
+
+                
               elif int(n2)>n1:
                print("demasiado grande")
-              else:
-                print('¡has ganado!')
-                print("Fin de partida")
-                break
-                sys.exit()    
 
-       
-       
+                
+              else: 
+                break
+     
+                sys.exit()    
+     if n1==n2:
+       intento = str(intento)
+       print("Has ganado")
+       print( '¡Has adivinado el número en ' + intento + ' intentos!')
+       print("FIN DE PARTIDA")
+    
   
   print(adivina_numero(n1))
   
