fa18208c9358336c0d9059f049128b98d7e72a2a
valuvilla <valuvp@hotmail.com>
2022-10-13 19:22:09 +0200

    cambios

---
+++ pedir_numero.py
@@ -0,0 +1,25 @@
+
+import sys
+
+from definirlimites import minimo, maximo
+
+invitacion=print("Hola")
+
+def pedir_numero(invitacion):
+
+    invitacion+= '{} entre {} y {}'.format(invitacion, minimo, maximo)
+
+    while True:
+        numero=input("Introduce un número")
+        try:
+            numero = int(numero)
+        except:
+            print("Elemento no válido", file=sys.stderr)
+            pass
+        else:
+            if minimo<=numero<=maximo:
+                break   
+            sys.exit()
+
+
+    
