47c29a06bf116419df588b8cc368d58e264b1acc
valuvilla <valuvp@hotmail.com>
2022-10-16 16:09:31 +0200

    modulos

---
+++ pedirnumero.py/__init__.py
@@ -0,0 +1,31 @@
+import sys
+
+
+valor=""
+def pedir_numero(valor):
+    while True:
+        valor=input("Introduce un valor: ")
+        try:
+            valor=int(valor)
+        except:
+            print("Valor no numérico", file=sys.stderr)
+        else:
+            break
+            sys.exit()
+    return valor
+
+def solicitar_intento(minimo=MIN1, maximo=MAX1):
+    invitacion="Adivine un número"
+    while True:
+        invitacion= "{} entre {} y {} incluídos".format(invitacion, minimo, maximo)
+        print(invitacion)
+        intento= pedir_numero(valor)
+        try:
+            minimo<=intento<=maximo
+        except:
+            print("{} no está entre {} y {}".format(intento,minimo,maximo ))
+            pass
+        else: 
+            sys.exit()
+            
+print(solicitar_intento(minimo=MIN1, maximo=MAX1)) 
