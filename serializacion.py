17d13727b7c9ce0d5e77ccb63e4dec1d7ae24a1d
valuvilla <valuvp@hotmail.com>
2022-10-17 00:13:34 +0200

    pickle

--- serializacion.py
+++ serializacion.py
@@ -4,10 +4,14 @@ from juego.juego import usuario, n_intentos
 datos=[]
 datos.append(usuario, n_intentos)
 
-ArchivoBinario - open("ArchivoLista", "wb")
+ArchivoBinario=open("ArchivoLista", "wb")
 
 pickle.dump(datos, ArchivoBinario)
 
 ArchivoBinario.close()
 
-Archivo2 - open()
+Archivo2=open("ArchivoLista", "rb")
+
+Lista2=pickle.load(Archivo2)
+
+print(Lista2)
