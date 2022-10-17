4d0bf3fd4066ffd0c868128a47a005b203e225c4
valuvilla <valuvp@hotmail.com>
2022-10-18 01:04:30 +0200

    comentarios

--- entrada_datos/entrada.py
+++ entrada_datos/entrada.py
@@ -1,20 +1,26 @@
 import sys
 import random
 
-from menu.menu import *
+from entrada_menu.menu import *
 
 
 def adivina_numero():
+    #mediante la función opciones definimos el nivel de dificultad
     minimo, maximo, intento_maximo = opciones()
+    #generamos aleatoriomente un numero a adivinar
     numero=random.randint(minimo, maximo)
+    
     n_intentos=0
+    #Bucle para evaluar el intento
     while n_intentos!=intento_maximo:
     
         n_intentos+=1
+        #llamamos a la funcion solicitar intentos.
         intento= solicitar_intento(minimo, maximo, intento_maximo)
 
         if intento<numero:
             print("demasiado pequeño", "\n")
+            #Acortara el rango
             minimo=intento+1
             print("Intento={}".format(n_intentos))
             victoria=False
@@ -22,6 +28,7 @@ def adivina_numero():
         elif intento>numero:
             print("demasiado grande", "\n")
             print("Intento={}".format(n_intentos))
+            #Acortara el rango
             maximo=intento-1
             victoria=False
                
@@ -30,18 +37,21 @@ def adivina_numero():
             print("Has adivinado el número en {} intento/s!".format(n_intentos))
             victoria=True
             break
-        
+
+    #en caso de no adivinar se mostara el  numero por pantalla
     if numero!=intento:
-        print("El numero pensado era {}".format(numero)) 
-        no_intento=True 
+        print("El numero pensado era {}".format(numero))
+         
+        
 
 
     return   victoria, minimo, maximo, n_intentos, intento_maximo
     
 
-
+#Funcion para evaluar que el numero dado por pantalla es un numero
 def pedir_numero():
     while True:
+        #Bucle infinito
         valor=input("Introduce un valor: ")
         try:
             valor=int(valor)
@@ -52,9 +62,13 @@ def pedir_numero():
             sys.exit()
     return valor
 
+#Funcion para evaluar  que el numero dado por pantalla esta en el rango dado
 def solicitar_intento(minimo, maximo, intento_maximo):
     invitacion="Adivine un número"
     while True:
+        #nucle infinito 
+
+
         invitacion= "{} entre {} y {} incluídos. Tiene como maximo {} intentos".format(invitacion, minimo, maximo, intento_maximo)
         print(invitacion)
         dato= pedir_numero()
@@ -94,7 +108,7 @@ def jugador(minimo, maximo, intento_maximo):
 
     return dato     
 """
-
+#Funciones que nos permite definir la cantidad de veces que queremos jugar
 SI=('si', 'Si', 'Yes', 'yes', 'verdadero', 'true', '1', 'YES', 'SI', 'VERDADERO', 'TRUE')
 def desea_jugar(invite):
     if input(invite) in SI:
--- juego/juego.py
+++ juego/juego.py
@@ -1,15 +1,25 @@
 
 from tabulate import tabulate
+#Para crear una lista de valores persistentes
 import pickle
 
-from entrada_datos.entrada import *
+from entrada_datos.__init__ import *
 
+#funcion responsable de cada juego
 def jugar_partida():
     usuario=input("Usuario: ")
     tabla=[["Usuario","Intentos"]]
+
     while True:
+
+        #Nos permite jugar varias veces
+        #bucle infinito
+
+        #llamamos a la funcion que nos permite jugar
+
         victoria, minimo, maximo, n_intentos, intento_maximo = adivina_numero()
 
+        #Intento de crear una tabla con los intentos
         tabla.append([usuario, n_intentos])
     
 
@@ -23,7 +33,8 @@ def jugar_partida():
 
         
 
-
+        #El bucle solo se rompera si se adivina el numero dentro de los intentos establecidos
+        #o se acaban los intentos
 
         if n_intentos==intento_maximo or victoria==True:
             break
